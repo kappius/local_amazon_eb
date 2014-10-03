ec5b4cd1e491a5caa29e034ad734c16f38fe01da
Diogo <diogo@medksaude.com.br>
2014-10-03 15:15:01 -0300

    added double quotes

--- supervisor/start.sh
+++ supervisor/start.sh
@@ -7,7 +7,9 @@ for d in ${WORKERS_DIR}; do
 	export IP=${d##*/};
 	watchmedo shell-command \
 	    --patterns="*.zip" \
-	    --command='xe snapshot-revert snapshot-uuid=${UUIDS[$COUNTER]};xe vm-start -u root vm=${NAMES[$COUNTER]};scp ${watch_src_path} ${WORKER_USER}@${IP}:${WORKER_MACHINE_DIR}/;rm -rf ${watch_src_path};' \
+	    --command="xe snapshot-revert snapshot-uuid=${UUIDS[$COUNTER]};xe vm-start -u root vm=${NAMES[$COUNTER]};scp '${watch_src_path}' ${WORKER_USER}@${IP}:${WORKER_MACHINE_DIR}/;rm -rf '${watch_src_path}';" \
+	    --wait=1 \
+	    --drop=1 \
 	    $d;
 	let COUNTER=COUNTER+1;
 	export COUNTER;
