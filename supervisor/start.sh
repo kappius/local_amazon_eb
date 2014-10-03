9508a71e1cdf7c694b3400a50aea1879fb562e93
Diogo <diogo@medksaude.com.br>
2014-10-03 14:16:43 -0300

    export variables

--- supervisor/start.sh
+++ supervisor/start.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
-COUNTER=0;
-UUIDS=($WORKERS_UUID);
-NAMES=($WORKERS_NAME);
+export COUNTER=0;
+export UUIDS=($WORKERS_UUID);
+export NAMES=($WORKERS_NAME);
+
 for d in ${WORKERS_DIR}; do 
+	export $d;
 	watchmedo shell-command \
 	    --patterns="*.zip" \
 	    --command='xe snapshot-revert snapshot-uuid=${UUIDS[COUNTER]};xe vm-start -u root vm=${NAMES[COUNTER]};scp ${watch_src_path} ${WORKER_USER}@${d}:${WORKER_MACHINE_DIR}/;rm -rf ${watch_src_path};' \
 	    $d;
 	let COUNTER=COUNTER+1;
+	export COUNTER;
 done;
