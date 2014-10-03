16a7f6387dc7d3a0e0a44cc95cabd2170987f9cb
Diogo <diogo@medksaude.com.br>
2014-10-03 14:34:23 -0300

    get ip

--- supervisor/start.sh
+++ supervisor/start.sh
@@ -4,7 +4,7 @@ export UUIDS=($WORKERS_UUID);
 export NAMES=($WORKERS_NAME);
 
 for d in ${WORKERS_DIR}; do 
-	export $d;
+	export IP=${d##*/};
 	watchmedo shell-command \
 	    --patterns="*.zip" \
 	    --command='xe snapshot-revert snapshot-uuid=${UUIDS[COUNTER]};xe vm-start -u root vm=${NAMES[COUNTER]};scp ${watch_src_path} ${WORKER_USER}@${d}:${WORKER_MACHINE_DIR}/;rm -rf ${watch_src_path};' \
