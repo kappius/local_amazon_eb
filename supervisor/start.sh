269dbd19766f4de63698ad5074ffc82babb62fe9
Diogo <diogo@medksaude.com.br>
2014-10-03 10:25:47 -0300

    now with exec to rename

--- supervisor/start.sh
+++ supervisor/start.sh
@@ -3,7 +3,7 @@ COUNTER=0;
 UUIDS=($WORKERS_UUID);
 NAMES=($WORKERS_NAME);
 for d in ${WORKERS_DIR}; do 
-	watchmedo shell-command \
+	exec -a $d watchmedo shell-command \
 	    --patterns="*.zip" \
 	    --command='xe snapshot-revert snapshot-uuid=${UUIDS[COUNTER]};xe vm-start -u root vm=${NAMES[COUNTER]};scp ${watch_src_path} ${WORKER_USER}@${d}:${WORKER_MACHINE_DIR}/;rm -rf ${watch_src_path};' \
 	    $d;
