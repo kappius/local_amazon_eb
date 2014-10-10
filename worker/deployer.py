c017353a022af6dd4c30e1a16b3b5a68b2f4d673
Diogo <diogo@medksaude.com.br>
2014-10-10 13:41:29 -0300

    removing watchdog

--- worker/deployer.py
+++ worker/deployer.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import time, shutil, os, glob, zipfile
-from watchdog.observers import Observer
-from watchdog.events import FileSystemEventHandler
+import time, shutil, os, glob, zipfile, sys
 from subprocess import call
 
 ROOT = os.environ['INSTALL_DIR']
@@ -10,32 +8,18 @@ POOL = os.environ['POOL_DIR']
 TRASH = os.environ['TRASH_DIR']
 DEPLOY = os.environ['DEPLOY_DIR']
 
-class DumpHandler(FileSystemEventHandler):
-    def on_any_event(self, event):
-
-        files = glob.iglob(os.path.join(POOL, "*.zip"))
-        for f in files:
-            if os.path.isfile(f):
-                try:
-                    with zipfile.ZipFile(os.path.join(POOL, f)) as zip:
-                        zip.extractall(DEPLOY)
-                    call(['pip', 'install', '-r', os.path.join(DEPLOY, 'requirements.txt')])
-                    shutil.move(os.path.join(POOL, f), TRASH)
-                except Exception, e:
-                    print e
-                    continue
-                call(['chown', '-R', 'www-data:www-data', DEPLOY])
-
-
-if __name__ == "__main__":
-    event_handler = DumpHandler()
-    observer = Observer()
-    observer.schedule(event_handler, path=POOL, recursive=False)
-    observer.start()
-
-    try:
-        while True:
-            time.sleep(1)
-    except KeyboardInterrupt:
-        observer.stop()
-    observer.join()
+while True:
+    files = glob.iglob(os.path.join(POOL, "*.zip"))
+    for f in files:
+        if os.path.isfile(f):
+            try:
+                with zipfile.ZipFile(os.path.join(POOL, f)) as zip:
+                    zip.extractall(DEPLOY)
+                call(['pip', 'install', '-r', os.path.join(DEPLOY, 'requirements.txt')])
+                shutil.move(os.path.join(POOL, f), TRASH)
+            except Exception, e:
+                print e
+                continue
+            call(['chown', '-R', 'www-data:www-data', DEPLOY])
+            sys.exit()
+    time.sleep(1)
