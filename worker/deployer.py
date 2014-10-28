bb0dca0b5a494713c47393f0345af62ab73c347e
Diogo <diogo@medksaude.com.br>
2014-10-28 15:45:31 -0200

    cnage subprocess

--- worker/deployer.py
+++ worker/deployer.py
@@ -36,7 +36,7 @@ def post_install():
     log = ''
     if POST_INSTALL_NAME:
         scp.get(POST_INSTALL_PATH, ROOT)
-        log += subprocess.check_output(os.path.join(ROOT, POST_INSTALL_NAME))
+        log += subprocess.check_output([os.path.join(ROOT, POST_INSTALL_NAME)])
     return log
 
 def add_envvars():
